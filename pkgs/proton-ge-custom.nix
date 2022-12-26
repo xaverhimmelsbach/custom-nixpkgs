533919cb90b36104a0a16666b7d524c3a50f748b
xaverhimmelsbach <xaver.himmelsbach@gmail.com>
2022-12-26 15:33:09 +0100

    Fix proton-ge-custom build

--- pkgs/proton-ge-custom.nix
+++ pkgs/proton-ge-custom.nix
@@ -1,13 +1,11 @@
-{ stdenv, fetchFromGitHub, ... }:
+{ stdenv, pkgs, ... }:
 stdenv.mkDerivation rec {
   pname = "proton-ge-custom";
   version = "GE-Proton7-43";
 
-  src = fetchFromGitHub {
-    owner = "GloriousEggroll";
-    repo = pname;
-    rev = version;
-    sha256 = "sha256-RZGJxxDW2GGRdfHN/HIocK4EPVQwyJlKIc++OPsZoPI=";
+  src = pkgs.fetchurl {
+    url = "https://github.com/GloriousEggroll/proton-ge-custom/releases/download/${version}/${version}.tar.gz";
+    sha256 = "sha256-owpe+spCfRY9H7yPqgbCIcnEOgNTtSeeEehTDsZNjQw=";
   };
 
   buildCommand = ''
