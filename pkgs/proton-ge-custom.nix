880d8493dcb0164e28c31f01552638bce3953aff
xaverhimmelsbach <xaver.himmelsbach@gmail.com>
2023-02-08 19:46:11 +0100

    Update Proton-GE-Custom to version 7-49

--- pkgs/proton-ge-custom.nix
+++ pkgs/proton-ge-custom.nix
@@ -1,11 +1,11 @@
 { stdenv, pkgs, ... }:
 stdenv.mkDerivation rec {
   pname = "proton-ge-custom";
-  version = "GE-Proton7-43";
+  version = "GE-Proton7-49";
 
   src = pkgs.fetchurl {
     url = "https://github.com/GloriousEggroll/proton-ge-custom/releases/download/${version}/${version}.tar.gz";
-    sha256 = "sha256-owpe+spCfRY9H7yPqgbCIcnEOgNTtSeeEehTDsZNjQw=";
+    sha256 = "sha256-T+7R+zFMd0yQ0v7/WGym2kzMMulUmATS/LCEQS8whiw=";
   };
 
   buildCommand = ''
