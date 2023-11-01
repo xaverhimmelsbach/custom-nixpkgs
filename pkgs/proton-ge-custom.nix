50e4fffa3eca083e7541a927a6c381709e952a3e
xaverhimmelsbach <xaver.himmelsbach@gmail.com>
2023-11-01 20:39:09 +0100

    Update proton-ge-custom

--- pkgs/proton-ge-custom.nix
+++ pkgs/proton-ge-custom.nix
@@ -1,11 +1,11 @@
 { stdenv, pkgs, ... }:
 stdenv.mkDerivation rec {
   pname = "proton-ge-custom";
-  version = "GE-Proton8-1";
+  version = "GE-Proton8-22";
 
   src = pkgs.fetchurl {
     url = "https://github.com/GloriousEggroll/proton-ge-custom/releases/download/${version}/${version}.tar.gz";
-    sha256 = "sha256-evxo0/RHRTRLYamMJxOUC1S+/4D40XJ41vNLpIQRooE=";
+    sha256 = "sha256-JBS1CFdiOCKLWwavx/o+TFHUPFAA/wygrFcyO9SK9cc";
   };
 
   buildCommand = ''
