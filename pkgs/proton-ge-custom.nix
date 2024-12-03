4274d242b5322b62b21eb6a097f9825a17477fa8
xaverhimmelsbach <xaver.himmelsbach@gmail.com>
2024-12-03 18:45:51 +0100

    Update proton-ge-custom to 9-20

--- pkgs/proton-ge-custom.nix
+++ pkgs/proton-ge-custom.nix
@@ -1,11 +1,11 @@
 { stdenv, pkgs, ... }:
 stdenv.mkDerivation rec {
   pname = "proton-ge-custom";
-  version = "GE-Proton9-10";
+  version = "GE-Proton9-20";
 
   src = pkgs.fetchurl {
     url = "https://github.com/GloriousEggroll/proton-ge-custom/releases/download/${version}/${version}.tar.gz";
-    hash = "sha256-bP42mbod19zxyT1gkxhLuK8F9VTowEvfa7lspk+tD1s=";
+    hash = "sha256-jDWZ4m5aUALhs/EhGwrFa/3dbrLE3Lrn2BAnGC7TbIk=";
   };
 
   buildCommand = ''
